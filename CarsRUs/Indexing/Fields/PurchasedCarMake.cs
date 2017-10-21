1be30b5b9eb1f381161898b8858e509f93054699
Daniel Møller Kristensen <phaserlich@outlook.dk>
2017-10-21 14:17:45 +0200

    Create computed field and facet for purchased car make.

---
+++ CarsRUs/Indexing/Fields/PurchasedCarMake.cs
@@ -0,0 +1,41 @@
+﻿using Sitecore;
+using Sitecore.Data.Fields;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.ComputedFields;
+using System.Collections.Generic;
+
+namespace CarsRUs.Indexing.Fields
+{
+    public class PurchasedCarMake : AbstractComputedIndexField
+    {
+        public override object ComputeFieldValue(IIndexable indexable)
+        {
+            var item = indexable as SitecoreIndexableItem;
+
+            if (item == null)
+                return null;
+
+            var purchasedMakes = new List<string>();
+
+            if (item.Item.TemplateID == Templates.Customer.ID)
+            {
+                var referrers = Globals.LinkDatabase.GetItemReferrers(item, false);
+
+                foreach (var referrer in referrers)
+                {
+                    var sourceItem = referrer.GetSourceItem();
+
+                    if (sourceItem.TemplateID == Templates.CarPurchase.ID)
+                    {
+                        foreach(var carItem in ((MultilistField)sourceItem.Fields[Templates.CarPurchase.Fields.Cars]).GetItems())
+                        {
+                            purchasedMakes.Add(carItem.Fields[Templates.Car.Fields.Make].Value);
+                        }
+                    }
+                }
+            }
+
+            return purchasedMakes;
+        }
+    }
+}
---
+++ CarsRUs/Templates.cs
@@ -0,0 +1,32 @@
+﻿using Sitecore.Data;
+
+namespace CarsRUs
+{
+    public struct Templates
+    {
+        public struct Customer
+        {
+            public static readonly ID ID = new ID("{F8CA835C-A7F5-4346-AC10-9983BE99632A}");
+        }
+
+        public struct CarPurchase
+        {
+            public static readonly ID ID = new ID("{17974EF8-F565-4716-A820-2E9DFC73A6A2}");
+
+            public struct Fields
+            {
+                public static readonly ID Cars = new ID("{3BB8925F-6299-42B8-9CD6-C4FFD767B5FF}");
+            }
+        }
+
+        public struct Car
+        {
+            public static readonly ID ID = new ID("{C3D6C1F7-E7D1-4E6B-A326-EC967D272354}");
+
+            public struct Fields
+            {
+                public static readonly ID Make = new ID("{48DA02A5-E4C2-452A-A4ED-25E92D80BD34}");
+            }
+        }
+    }
+}
