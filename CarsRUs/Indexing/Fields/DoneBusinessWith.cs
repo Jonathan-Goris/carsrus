f18928d9c8d077579632e38c97dfe9228dc461cf
Daniel Møller Kristensen <phaserlich@outlook.dk>
2017-10-21 16:07:10 +0200

    Create computed field and facet for purchased sales person.

---
+++ CarsRUs/Indexing/Fields/DoneBusinessWith.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore;
+using Sitecore.Data.Fields;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.ComputedFields;
+using System.Collections.Generic;
+
+namespace CarsRUs.Indexing.Fields
+{
+    public class DoneBusinessWith : AbstractComputedIndexField
+    {
+        public override object ComputeFieldValue(IIndexable indexable)
+        {
+            var item = indexable as SitecoreIndexableItem;
+
+            if (item == null)
+                return null;
+
+            var doneBusinessWith = new List<string>();
+
+            if (item.Item.TemplateID == Templates.Customer.ID)
+            {
+                var referrers = Globals.LinkDatabase.GetItemReferrers(item, false);
+
+                foreach (var referrer in referrers)
+                {
+                    var sourceItem = referrer.GetSourceItem();
+
+                    if (sourceItem.TemplateID == Templates.CarPurchase.ID)
+                    {
+                        doneBusinessWith.Add(((ReferenceField)sourceItem.Fields[Templates.CarPurchase.Fields.SalesPerson]).TargetItem?.Fields[Templates.SalesPerson.Fields.Name].Value);
+                    }
+                }
+            }
+
+            return doneBusinessWith;
+        }
+    }
+}
--- CarsRUs/Templates.cs
+++ CarsRUs/Templates.cs
@@ -16,6 +16,7 @@ namespace CarsRUs
             public struct Fields
             {
                 public static readonly ID Cars = new ID("{3BB8925F-6299-42B8-9CD6-C4FFD767B5FF}");
+                public static readonly ID SalesPerson = new ID("{B772EE34-2BCC-430C-B86C-4FB17A7A2B00}");
             }
         }
 
@@ -27,6 +28,16 @@ namespace CarsRUs
             {
                 public static readonly ID Make = new ID("{48DA02A5-E4C2-452A-A4ED-25E92D80BD34}");
                 public static readonly ID Model = new ID("{C3010EFA-06D1-4954-895B-48E4E47EE79B}");
+            }
+        }
+
+        public struct SalesPerson
+        {
+            public static readonly ID ID = new ID("{BFEA2529-75FD-4D03-8604-00D1BB4EAD10}");
+
+            public struct Fields
+            {
+                public static readonly ID Name = new ID("{08F5519D-A531-4A78-A99F-E53EE5C46035}");
             }
         }
     }
