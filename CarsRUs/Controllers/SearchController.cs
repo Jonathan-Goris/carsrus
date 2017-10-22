708192cb09e7a8df21657844221deaa9643e87cb
Daniel Møller Kristensen <phaserlich@outlook.dk>
2017-10-22 11:33:27 +0200

    Create search rendering.

---
+++ CarsRUs/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace CarsRUs.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
